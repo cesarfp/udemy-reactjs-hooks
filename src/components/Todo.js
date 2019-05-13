0bf669d1518330d40448970316e44345737a229a
César <cesar.franco@4thsource.com>
2019-05-13 17:25:10 -0500

    class 459

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,10 @@
 import React, {useState} from 'react'
 
 const todo = (props) => {
-	const inputState=useState('')
+	const [todoName,setTodoName]=useState('')
 
 	const inputChangedHandler = (event) => {
-		inputState[1](event.target.value)
+		setTodoName(event.target.value)
 	}
 
 	return <React.Fragment>
@@ -12,7 +12,7 @@ const todo = (props) => {
 			type="text" 
 			placeholder="Todo" 
 			onChange={inputChangedHandler} 
-			value={inputState[0]}/>
+			value={todoName}/>
 		<button type="button">Add</button>
 		<ul/>
 	</React.Fragment>

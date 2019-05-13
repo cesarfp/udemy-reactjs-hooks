1f15e41006ec92a27e8686569c41a5577fa21d8e
César <cesar.franco@4thsource.com>
2019-05-13 17:36:05 -0500

    class 460

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,9 +2,14 @@ import React, {useState} from 'react'
 
 const todo = (props) => {
 	const [todoName,setTodoName]=useState('')
+	const [todoList,setTodoList]=useState([])
 
 	const inputChangedHandler = (event) => {
 		setTodoName(event.target.value)
+	}
+
+	const todoAddHandler = () => {
+		setTodoList(todoList.concat(todoName))
 	}
 
 	return <React.Fragment>
@@ -13,8 +18,14 @@ const todo = (props) => {
 			placeholder="Todo" 
 			onChange={inputChangedHandler} 
 			value={todoName}/>
-		<button type="button">Add</button>
-		<ul/>
+		<button 
+			type="button"
+			onClick={todoAddHandler}>Add</button>
+		<ul>
+			{todoList.map(todo=>(
+				<li key={todo}>{todo}</li>
+			))}
+		</ul>
 	</React.Fragment>
 	
 }
